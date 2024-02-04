4403e5e7d0a90c752edb2d91298cfdd3f2347322
ayatri4545 <154301569+ayatri4545@users.noreply.github.com>
2024-02-04 15:50:21 +0530

    Add files via upload

---
+++ Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Pixbay from "./Googlemeet/Project2"
+
+const App = () => {
+  return (
+    <div>
+      <Pixbay/>
+      
+    </div>
+  )
+}
+
+export default App
